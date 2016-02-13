4f562a2733e4f75d3963ad89b82687888f9c2b3e
Beck Chen <bcfchen@gmail.com>
2016-02-12 23:10:13 -0800

    [Beck] prep for live

--- app/app.js
+++ app/app.js
@@ -31,25 +31,8 @@ angular
   })
 
   .config(function ($stateProvider, $urlRouterProvider, TwilioProvider, TwilioVerificationProvider) {
-    Stripe.setPublishableKey('pk_test_eiMfC0If1yWAHJMOSWFTz1FK');
+    Stripe.setPublishableKey('pk_live_570nSYPKpEFawxjct8tu4u9Z');
 
-    /* test account */
-        // setup Twilio 
-    TwilioProvider.setCredentials({
-        accountSid: 'ACe79928940d39103df64d9bac1fd06a9f',
-        authToken: '839a92ea384334275a5871970b5be354'
-    });
-
-    TwilioVerificationProvider.setFromNumber('+19252415828');
-
-    /* prod account */
-    // // setup Twilio 
-    // TwilioProvider.setCredentials({
-    //     accountSid: 'AC8b36caff6e2efe2cc52ba9c3adca65aa',
-    //     authToken: 'd7487cf487722e6167895d74e11a70d0'
-    // });
-
-    // TwilioVerificationProvider.setFromNumber('+14152756413');
     $urlRouterProvider.otherwise("/booking");
      $stateProvider
             .state('booking', {
