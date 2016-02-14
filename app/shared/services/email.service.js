8b21594e69b38999ab1837d8c1ed24bfa05c9a22
Beck Chen <bcfchen@gmail.com>
2016-02-14 08:02:04 -0800

    [Beck] forgot to include emailService

---
+++ app/shared/services/email.service.js
@@ -0,0 +1,26 @@
+(function() {
+
+    angular
+        .module("rare")
+        .factory("emailService", ["$q", "$http", "constants", "$firebaseAuth", "userBuilder", emailService]);
+
+    function emailService($q, $http, constants, $firebaseAuth, userBuilder) {
+        var service = {
+            sendEmail: sendEmail
+        };
+
+        return service;
+
+        /* method implementations */
+
+        function sendEmail(user, appointment) {
+            return $http.post(constants.SERVER_URL + "/send-email", {
+                user: user,
+                appointment: appointment
+            }).then(function(response) {
+                return response;
+            });
+        }
+
+    }
+}());
